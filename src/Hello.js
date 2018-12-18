6870d72af08b306fb82eeda8f01d9719e30add36
Matheus Guimarães <matheusguimr@gmail.com>
2018-12-18 16:02:34 -0300

    Add Hello JS and CSS file

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Hello.css'
+
+
+class Hello extends Component {
+	render() {
+		return (
+			<div className='f1 tc'>
+				<h1>Hello World</h1>
+				<p>{this.props.greeting}</p>
+			</div>
+ 		);
+ 	}
+}
+
+export default Hello;
