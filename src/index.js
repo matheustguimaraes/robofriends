7d9102b84cc7f2e754e0ed79141c7aac021f6678
Matheus Guimarães <matheusguimr@gmail.com>
2018-12-18 20:14:33 -0300

    Delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
-ReactDOM.render(<Hello greeting={'Hello' + 'React ninja'} />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+				<div>
+					<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+					<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+					<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+				</div>,
+document.getElementById('root'));
+serviceWorker.register();
