1ac54e1ecafc8afad21889bf8e0097e4cebc7688
Matheus Guimarães <matheusguimr@gmail.com>
2018-12-24 13:01:47 -0300

    Add redux-logger

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,6 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js'
 
 export const setSearchField = (text) => {
-  // console.log(text);
   return {
     type: 'CHANGE_SEARCH_FIELD',
     payload: text
