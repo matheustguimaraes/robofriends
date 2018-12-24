c4b60305ae7e0a4a4aaf54a09f9fdce2891b9e30
Matheus Guimarães <matheusguimr@gmail.com>
2018-12-24 12:13:33 -0300

    Wrap App with Providerand add redux imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import  { searchRobots } from './reducers';
 import 'tachyons';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+const store = createStore(searchRobots);
+
+ReactDOM.render(<Provider store={store}>
+                  < App />
+                </Provider>,
+                document.getElementById('root'));
 serviceWorker.register();
