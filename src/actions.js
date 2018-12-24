0badc45d3d1e1f8b4e6c1bfcd9b943bd2d4338f0
Matheus Guimarães <matheusguimr@gmail.com>
2018-12-24 15:07:58 -0300

    Edit project to handle async actions

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,19 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js'
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from './constants.js'
 
-export const setSearchField = (text) => {
-  return {
-    type: 'CHANGE_SEARCH_FIELD',
+export const setSearchField = (text) => ({
+    type: CHANGE_SEARCH_FIELD,
     payload: text
-    }
-  }
+  })
+
+export const requestRobots = () => (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING })
+  fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
+}
